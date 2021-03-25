5656ac6cfa256ce0a0761d379244af82acc20f06
Robert E. Novak <sailnfool@gmail.com>
2021-03-25 05:58:02 -0700

    changed the yesfsdir...
    to an environment variable.
    chaned FILECOUNT to an environment
    variable
    included func.regex
    
    Invoked parallel execution of the yes1 script to convert an individual
    file to a hashed object.  We may have to add -j to increase the
    parallelism of this to increase CPU and I/O utilization while running
    these scripts.

--- scripts/yespara.sh
+++ scripts/yespara.sh
@@ -4,6 +4,8 @@ source yfunc.hashdirpath
 source yfunc.maketop
 source yfunc.put_nhid
 source func.errecho
+source func.regex
+
 USAGE="${0##*/} [-h] [-d] <directory> [ <rootpath> ]\n
 \t<directory>\tThe directory that will be converted to a YesFS\n
 \t\t\tfile system prototype using b2sum cryptographic hash.\n
@@ -33,9 +35,10 @@ optionargs="hdv"
 NUMARGS=2
 debug=0
 verbose=1
-yesfsdir=/hashes
-FILECOUNT=0
-re_hexnumber='^[0-9a-f][0-9a-f]*$'
+export YesFSdir=${YesFSdir:=/hashes}
+func_putcounter "${FILECOUNT.lock}" "${FILECOUNT.file}" 0
+export FILECOUNT=$(func_getcounter "${FILECOUNT.lock}" \
+  "${FILECOUNT.file}")
 
 while getopts ${optionargs} name
 do
@@ -67,7 +70,7 @@ topdir="$1"
 shift
 if [ $# -gt 0 ]
 then
-  yesfsdir="$1"
+  YesFSdir="$1"
 fi
 
 if [ ! -d "${topdir}" ]
@@ -76,219 +79,8 @@ then
 	exit 0
 fi
 
-maketop "${yesfsdir}"
+maketop "${YesFSdir}"
 
 [ ${verbose} -eq 1 ] && echo $(date '+%T')
 
 find . -type f -print | parallel yes1 {}
-
-
-b2sum "$dirname"/* 2> /dev/null >/${TMPDIR}/thislist
-
-while read -r hashline
-do
-	####################
-	# A line beginning with "Failed" is a directory or
-	# other special file.  Ignore
-	####################
-	if [ "${hashline:0:${#FAILEDTO_HASH_string}}" == "${FAILEDTO_HASH_string}" ]
-	then
-		####################
-		# Not sure why this is two steps, but it works.
-		####################
-		dirstring="${hashline:${#FAILEDTO_HASH_string}}"
-		subdirname="${dirstring:0:${#dirstring}-1}"
-		
-		####################
-		# Recursively descend.
-		# Later, this can be made a background process.
-		####################
-		sevenb2b "${dirname}/${subdirname}" "$2"
-	fi
-
-	filename=${hashline:130}
-	if [ ! -f "${filename}" ]
-	then
-		echo "filename=${filename} is not a file"
-		exit 0
-	fi
-
-	####################	
-	# nhid   is the name hash ID of the file: NHID Daka $nhid
-	# p_nhid represents the directory path in the local file
-	#        system where we will place the NHID object.
-	# f_nhid is the full path ${p_nhid}/${nhid}
-	#
-	# Note that for the moment we are "punting" on the notion of
-	# creating directories that make up the "$(dirname ${filename})"
-	# In order to do this properly, we not only have to insure that
-	# the objects exist in the full path, but add appropriate sub
-	# directories and at the last element add the filename as a
-	# member of the leaf/edge directory.
-	####################	
-	nhid=$(put_nhid "${filename}" "${yesfsdir}" "${timestamp}" \
-	    "${CHUNKLOG}")
-
-
-	((FILECOUNT++))
-	p_nhid=$(hashdirpath ${nhid})
-	f_nhid="${p_nhid}/${nhid}"
-	((FILECOUNT++))
-
-	####################	
-	# Get the content hash ID of the file: CHID
-	# and the filename
-	####################	
-	chid=${hashline:0:128}
-	p_chid=$(hashdirpath ${chid})
-	f_chid="${p_chid}/${chid}"
-
-	bhid=$(put_firstbackref ${nhid})
-	if [ -f "${f_chid}" ]
-	then
-		previous_chid=""
-	fi
-	####################	
-	# This next step should be the process that breaks
-	# the file into multiple chunks and ties each of those
-	# chunks back to the nhid as they are created, building
-	# the basis for the chunk list that is at the end of
-	# the manifest for the object.  For now we simplify
-	# by turning the object into a single chunk.
-	####################
-	chunk_mani[0]=echo -e "${HASHTYPE}\t${SUFFIX[MANI]}"
-	chunk_mani[1]=echo -e "${PREVIOUS}\t0"
-	chunk_mani[2]=echo -e "${BACKREF}\t${bhid}"
-
-	
-	CHUNKID="${f_chid}.CHID"
-	METAID="${f_chid}.METACHUNK"
-	METAACCESSID="${f_chid}.METACHUNKACCESS"
-	if [ -f "${CHUNKID}" ]
-		echo -e "${chid}\tCHID\tB2\tACCESS\t${timestamp}" >> ${CHUNKLOG}
-		if [ ! -f ${METAACCESSID} ]
-		then
-			stderrecho "${METAID} not found"
-			exit 1
-		fi
-		while read metaline
-		do
-			ATR_string="ACCESS_TIME_RECORDS"
-			if [ "${metaline:0:${#ATR_string}}" = "${ATR_string}" ]
-			then
-				meta_access_id="${metaline:$((${#ATR_string}+1))}"
-			fi
-		done < ${METAACCESSID}
-		meta_accessid[0]="PREVIOUS_ACCESS_METACHUNKACCESS\t${meta_access_id}"
-		meta_accessid[1]="TIME\t${timestamp}"
-		meta_accessid[2]="USERID\t$(uid -u)"
-		meta_accessid[3]="GROUPID\t$(uid -g)"
-		
-	else
-		cp "${filename}" ${p_chid}/${chid}.CHID
-		echo -e "${chid}\tCHID\tB2\tCREATE\t${timestamp}" >> ${CHUNKLOG}
-		meta_accessid[0]="PREVIOUS_ID\t0"
-		meta_accessid[1]="TIME\t${timestamp}"
-		meta_accessid[2]="USERID\t$(uid -u)"
-		meta_accessid[3]="GROUPID\t$(uid -g)"
-	fi
-	for i in $(seq 0 3)
-	do
-		echo -e ${meta_accessid[$i]} >> ${METAACESSID}
-	done
-	fi
-
-	####################	
-	# Create the speculative backreference that ties the
-	# name to the chunk.
-	####################	
-	create_spec_back_ref ${chid} ${nhid}
-
-	####################	
-	# See hashdirpath to see how the directories
-	# are setup as prefixes for the hash names
-	# Set the Path for the NHID
-	# The Manifest has the suffix .MANIFEST
-	# The object containing the file name has the suffix .NHID
-	####################
-	p_nhid=$(hashdirpath ${nhid})
-	manid=${p_nhid}/${nhid}.MANIFEST
-	fullnhid=${p_nhid}/${nhid}.NHID
-	ldir="${filename%/*}"
-	mkdir -p "${yesfsdir}/${ldir}"
-	echo "${nhid}" >> "${yesfsdir}/${filename}"
-
-	####################
-	# If the NHID already exists it means we have a prior
-	# version of this name.
-	####################
-	if [ -r ${fullnhid} ]
-	then
-
-		####################
-		# A prior NHID means a prior MANIFEST  a safety
-		# check here would be a good idea.
-		# Take the hash of the prior manifest.  This
-		# hash will be placed in the new MANIFEST.
-		# Retrieve the version number of the object
-		# from the prior manifest
-		####################
-		prevmnidhash=$(b2sum ${manid})
-		prevmnid=${prevmnidhash:0:128}
-		p_mnid=$(hashdirpath ${prevmnid})
-		pmanid=${p_mnid}/${prevmnid}
-		prev_objid=$(awk -F'\t' '/OBJECTVERSION/{print $2}' < ${manid})
-		((object_version=prev_objid + 1))
-		mv ${manid} ${pmanid}
-		echo -e "PREVMANIFEST\t${prevmnid}" >${manid}
-	else
-
-		####################
-		# First Manifest for the object, set the 
-		# PREVMANIFEST to NULL and the object version
-		# to Zero
-		####################
-		echo -e "PREVMANIFEST\t0" > ${manid}
-		object_version=0
-	fi
-
-	####################
-	# Fill the Manifest.  This is currently missing the 
-	# chunks for the file contents which belongs here.
-	####################
-	echo -e "OBJECTVERSION\t${object_version}" >> ${manid}
-	echo -e "MANIFESTVERSIONMAJOR\t0" >> ${manid}
-	echo -e "MANIFESTVERSIONMINOR\t1" >> ${manid}
-	echo -e "NHID\t${chid}\n" >> ${manid}
-	echo -e "NAME\t${filename}" >> ${manid}
-	[ ${debug} -eq 1 ] && cat ${manid}
-
-	####################
-	# Create the NHID with the name and the CHID of the 
-	# object contents.
-	####################
-	echo -e "NHID\t${chid}\n" >> ${p_nhid}/${nhid}.NHID
-	echo -e "NAME\t${filename}" >> ${p_nhid}/${nhid}.NHID
-
-	####################
-	# Create a chunk ID for the content chunk that points
-	# pack to the name.  This is not correct as instantiated
-	# and probably should be dropped as redundant.  Multiple
-	# identical files will successively wipe this out.
-	# An alternate implementation would be to create a 
-	# linked list like the manifests.
-	####################
-	p_chid=$(hashdirpath ${chid})
-	echo -e "NAME\t${filename}" >> ${p_chid}/${chid}.CHID
-
-
-	[[ ${verbose} -eq 1 && \
-		$(expr ${FILECOUNT} % 100) -eq 0 ]] && \
-		echo -n "."
-	[[ ${verbose} -eq 1 && \
-		$(expr ${FILECOUNT} % 7000) -eq 0 ]] && \
-		{ \
-			echo ""; echo "$(date '+%T') ${FILECOUNT}"
-		}
-done < ${TMPDIR}/thislist
-wait
