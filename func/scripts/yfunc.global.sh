c4340589b1fa23d9bbb760ba0f178e90dd031b71
Robert E. Novak <sailnfool@gmail.com>
2022-05-28 09:11:11 -0700

    Make sure we export the environment information

--- func/scripts/yfunc.global.sh
+++ func/scripts/yfunc.global.sh
@@ -36,26 +36,40 @@ then
 # of the cryptographic hash function are logged in the CHUNKLOG
 #
 ########################################################################
-YESFS=/YesFS
-HASHES=${YESFS}/.hash
-CHUNKLOG=${YESFS}/.chunklog
-FAILEDTOHASH_string="Failed to hash \`"
+export YesFS=/home/rnovak/DropboxYesFS
+export HASHES=${YESFS}/.hash
+export CHUNKLOG=${YESFS}/.chunklog
+export FAILEDTOHASH_string="Failed to hash \`"
 
-FILECOUNT_lock="${TESTDIR}/FILECOUNT.lock"
-FILECOUNT_file="${TESTDIR}/FILECOUNT.file"
+export FILECOUNT_lock="${YESFS}/FILECOUNT.lock"
+export FILECOUNT_file="${YESFS}/FILECOUNT.file"
 
-####################	
+########################################################################
 # This timestamp will create a new time stamp each
 # time it is referenced.  The Time stamp is represented
 # in the format of the ISO 8601-2:2019 interchange format
+# as specified in "man date" the ISO format with nanosecond resolution
+# is requested.
 #
 # By removing the '\' at the beginning of the string
 # this would become a single timestamp for all elements
 # of the object creation, which may be desirable.
 #
 # This should be in the format of the AWS S3 timestamp
-####################	
+########################################################################
 timestamp="\$(date -u -Ins)"
+
+########################################################################
+# Create the associative arrays which will hold the canonical index
+# of each known and support cryptographic hash type.
+#
+# The files used to create these arrays are documented in
+# Create_canonical.sh
+########################################################################
+declare -a num2hash
+declare -A hash2num
+declare -A hash2bin
+declare -a num2bits
 
 b2len=128
 b2file=130
@@ -64,23 +78,23 @@ b2file=130
 # Suffixes
 ########################################################################
   # Chunk Hash ID, normally elided, absence implies this
-CHID=1
+export CHID=1
   # Name Hash ID
-NHID=2
+export NHID=2
   # Meta data affiliated with a chunk
-META=3
+export META=3
   # A back reference for a chunk.  These are speculative or confirmed
-BACK=4
+export BACK=4
   # A Manifest for an object
-MANI=5
+export MANI=5
   # Chunk Meta data
-CHUNKMETA=6
+export CHUNKMETA=6
   # Chunk Access data
-CHUNKACCESS=7
+export CHUNKACCESS=7
   # Chunk Manifest
-CHUNKMANI=8
+export CHUNKMANI=8
   # Name metadata
-NAMEMETA=9
+export NAMEMETA=9
 
 declare -a SUFFIX
 SUFFIX=( ${CHID} ${NHID} ${META} ${BACK} ${MANI} ${CHUNKMETA} \
@@ -224,7 +238,7 @@ USEDHASHES="USEDHASHES"	# The number of hashes in use for this object.
 
 # Rethinking this entire section.  The in-memory version is an
 # extensible array.  When written to storage, this is an array of
-# size "USEDHASHES" that contains all fo the pairs of offsets and
+# size "USEDHASHES" that contains all of the pairs of offsets and
 # chunk hashes.  This is on local store in the originating machine
 # only.  As the manifest is prepared for "put" to other storage
 # destinations, an isomorphic representation of this manifest may
@@ -238,23 +252,21 @@ OFFSET_0=""		# Offset to the [0] chunk of the object
 			# Chunk Manifests, then there are no bounds on 
 			# the size of an object since an object may have
 			# a tree of indefinite depth of chunk manifests
-			# These numbers are represented by up to 20
-			# character decimal numbers
-OFFSET_1		# Offset to the [1] chunk of the object
-OFFSET_2		# Offset to the [2] chunk of the object
-OFFSET_3		# Offset to the [3] chunk of the object
-OFFSET_4		# Offset to the [4] chunk of the object
-OFFSET_5		# Offset to the [5] chunk of the object
-OFFSET_6		# Offset to the [6] chunk of the object
-OFFSET_7		# Offset to the [7] chunk of the object
+OFFSET_1=""		# Offset to the [1] chunk of the object
+OFFSET_2=""		# Offset to the [2] chunk of the object
+OFFSET_3=""		# Offset to the [3] chunk of the object
+OFFSET_4=""		# Offset to the [4] chunk of the object
+OFFSET_5=""		# Offset to the [5] chunk of the object
+OFFSET_6=""		# Offset to the [6] chunk of the object
+OFFSET_7=""		# Offset to the [7] chunk of the object
 HASH_0=""		# HASH of the first [0] chunk of the object
-HASH_1			# HASH of the [1] chunk of the object
-HASH_2			# HASH of the [2] chunk of the object
-HASH_3			# HASH of the [3] chunk of the object
-HASH_4			# HASH of the [4] chunk of the object
-HASH_5			# HASH of the [5] chunk of the object
-HASH_6			# HASH of the [6] chunk of the object
-HASH_7			# HASH of the [7] chunk of the object
+HASH_1=""			# HASH of the [1] chunk of the object
+HASH_2=""			# HASH of the [2] chunk of the object
+HASH_3=""			# HASH of the [3] chunk of the object
+HASH_4=""			# HASH of the [4] chunk of the object
+HASH_5=""			# HASH of the [5] chunk of the object
+HASH_6=""			# HASH of the [6] chunk of the object
+HASH_7=""			# HASH of the [7] chunk of the object
 ########################################################################
 # CHUNKMANI Manifest Metadata for an object
 ########################################################################
@@ -284,5 +296,5 @@ HASH_00=""		# HASH of the first [00] chunk of the object
 			# .
 			# .
 HASH_FF=""		# HASH of the first [FF] chunk of the object
-fi # if [ -z "${__yfunc_global}" ]
 
+fi # if [ -z "${__yfunc_global}" ]
