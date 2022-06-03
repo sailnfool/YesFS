360777b82b8e829537ae2863eabc3516bc93f38d
Robert E. Novak <sailnfool@gmail.com>
2022-06-03 02:01:37 -0700

    Complete re-write

--- yfunc/scripts/yfunc.put_nhid.sh
+++ yfunc/scripts/yfunc.put_nhid.sh
@@ -18,42 +18,70 @@ then
 # Change all usage of b2sum and its lengh to functions of the 
 # Associative arrays
 #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
+
+    ####################################################################
+    # First we compute the hash of the full name of the file.  This
+    # must include the complete YesFS path from the root of the
+    # YesFS filesystem (YesFS_HASH) to the current file.  This means
+    # that we need the context of the YesFS tenant, the path name
+    # within the tenant space and the name of the file itself.  To do
+    # this we need YesFS versions of pwd, i.e. ypwd as well.  There
+    # are also files which are part of the YesFS configuration which 
+    # belong to the tenant __YesFS and are found in __YesFS/etc files
+    # as the data tables that manage the file system.  In terms of
+    # passed parameters, we will assume for simplicity that the
+    # filename passed to this function includes all of the correct
+    # elements in the filename path.
+    ####################################################################
+    # The num2bin array contains the mapping from the CryptoID number
+    # to the local system's executable file which will take stdin
+    # piped input of a file/string and return a non-canonical hash
+    # code "  -" as the name of the hashed data.
+    ####################################################################
 		nhidhash="$(echo "${filename}" | ${num2bin[${cryptoID}]})"
-    nhid="${cryptoID}:${nhidhash:0:$((${num2bits[${cryptoID}]}*2))}"
-		p_nhid=hashdirpath ${nhid}
+
+    ####################################################################
+    # We take the hex encoded hash string and prefix it with the
+    # cryptoID of the hash program, a colon and then the number of
+    # character encoded hex characters representing the hash encoding
+    ####################################################################
+    nhid="${cryptoID}:${nhidhash:0:${num2hexdigits[${cryptoID}]}}"
+    p_nhid=$(hashdirpath ${nhid})
 		f_nhid="${p_nhid}/${nhid}"
 
-		if [ -r "${f_nhid}.${SUFFIX[CHID]}" ]
-		then
-			stderrecho "Duplicate Backreference ${bhid}"
-		fi
-		echo "${filename}" > "${f_nhid}.${SUFFIX[CHID]}"
-
-		echo -e "${nhid}\t$SUFFIX[CHID]}\t${timestamp}" \
-			>> ${CHUNKLOG}
-		backref[0]="HASHTYPE\tBACK"
-		backref[1]="PREVIOUS\t0"
-		backref[2]="NEXT\t0"
-		backref[3]="NAMEHASH\t${nhid}"
-		backref[4]="BACKTIME\t${timestamp}"
-		backref[5]="SPEC\tTRUE"
-		tmpback=${TMPDIR}.${filename}.BACK
-		rm -f ${tmpback}
-		for i in $(seq 0 5)
-		do
-			echo -e ${backref[$i}]} >> ${tmpback}
-		done
-		bhid=$(b2sum ${tmpback})
-		p_bhid=hashdirpath ${bhid}
-		f_bhid="${p_bhid}/${bhid}"
-		mv ${tmpback} ${f_bhid}
-		echo "${bhid}\t$SUFFIX[BACK]}\t${HASHCODE}\t${timestamp}" \
-			>> ${CHUNKLOG}
-
-		####################
-		# This is where we put the code for the global put
-		####################
-		echo "${nhid}"
+    NHID["CHUNKTYPE"]="NHID"
+    NHID["fullname"]=$(echo $filename)
+    MANI["CHUNKTYPE"]="MANIFEST"
+    MANI["prevmani"]=""
+    BACKR["CHUNKTYPE"]="BACKREFERENCE"
+    BACKR["name"]=${f_nhid}
+    BACKR["ctime"]="$(date -U -Ins)"
+    BACKR["speculative"]="TRUE"
+    BACKR["next"]=""
+    BACKR["prev"]=""
+    MANI["back"]=$(put_backr ${cryptoID})
+    MANI["objlen"]=$(stat -c "%s" ${filename})
+    MANI["ctime"]="$(date -u -Ins)"
+    chunkhash=$(${num2bin[${cryptoID}]} $filename)
+    chid="${cryptoID}:${chunkhash:0:${num2hexdigits[${cryptoID}]}}"
+    p_chid=$(hashdirpath ${chid})
+    f_chid="${p_chid}/${chid}"
+    cp ${filename} ${f_chid}
+    MANI["hash0"]="${chid}"
+    MANI["off0"]=0
+    for i in $(sed 1 7)
+    do
+      MANI["hash${i}"]=0
+      MANI["off${i}"]=0
+    done
+    NHID["objmani"]=$(put_mani ${cryptoID})
+    NAMEMETA["CHUNKTYPE"]="NAMEMETA"
+    NAMEMETA["ownerid"]="$(stat -c "%u" ${filename})"
+    NAMEMETA["groupid"]="$(stat -c "$g" ${filename})"
+    NAMEMETA["perm"]="$(stat -c "%A" ${filename})"
+    NAMEMETA["next"]=""
+    NHID["namemeta"]="$(put_namemeta ${cryptoID})"
+    echo "${nhid}"
 	}
 	export put_nhid
 fi # if [-z "${__yfunc_put_nhid}" ]
