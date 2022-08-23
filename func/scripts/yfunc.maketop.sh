cce010bc98bcf52a2372f856900add322d9dd248
Robert E. Novak <sailnfool@gmail.com>
2022-08-23 05:52:21 -0700

    Needs serious rework

---
+++ func/scripts/yfunc.maketop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ -z "${__yfunc_maketop}" ]
+then
+	export __yfunc_maketop=1
+
+	function maketop {
+		YesFSdir=$1
+		TMPTOP="/tmp"
+		YTMPDIR="${TMPTOP}/${YesFSdir##*/}.$$.dir"
+		HASHES="${YesFSdir}/.hash"
+		CHUNKLOG="${YesFSdir}/.chunklog"
+		if [ ! -d "${YTMPDIR}" ]
+		then
+			mkdir -p ${YTMPDIR}
+			chmod 777 ${YTMPDIR}
+		fi
+		if [ ! -d "${HASHES}" ]
+		then
+			mkdir -p ${HASHES}
+			chmod 777 ${HASHES}
+		fi
+		if [ ! -d "${CHUNKLOG}" ]
+		then
+			mkdir -p ${CHUNKLOG}
+			chmod 777 ${CHUNKLOG}
+		fi
+
+		export TMPTOP YTMPDIR HASHES CHUNKLOG
+	}
+	export maketop
+fi # if [-z "${__yfunc_maketop}" ]
